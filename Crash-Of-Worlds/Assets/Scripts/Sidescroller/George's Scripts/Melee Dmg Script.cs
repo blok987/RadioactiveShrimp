345d6975020ce6c9c970a981d01c8908842dea28
bloked987 <bloked987@gmail.com>
2025-11-20 09:50:22 -0800

    Worked on Melee Damage and attacks

---
+++ Crash-Of-Worlds/Assets/Scripts/Sidescroller/George's Scripts/Melee Dmg Script.cs
@@ -0,0 +1,62 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class MeleeScript : MonoBehaviour
+{
+    public bool PlayerAttack;
+    public bool PlayerContact;
+    public bool PlayerSwordDash;
+    public int PlayerSwordDMG;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            PlayerAttack = true;
+            if (PlayerContact == true)
+            {
+                PlayerSwordDMG = 5;
+                // make enemy take damage
+            }
+            else PlayerSwordDMG = 0;
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            PlayerContact = true;
+        }
+        
+    }
+
+    //Determines whether player is in contact with enemy 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            PlayerContact = false;
+        }
+        
+    }
+
+
+
+
+    // Determines the Damage the player deals on Mouse Down
+    private void OnMouseDown()
+    {
+        
+    }
+}
+
+
