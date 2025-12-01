52155e74c61100dd59bf335d0008f2886b25be62
Tyler Musgrove <1725402@mukilteo.wednet.edu>
2025-12-01 08:13:01 -0800

    Player sprites folder and gamemanager script started

---
+++ Crash-Of-Worlds/Assets/Scripts/Tyler/GameManagerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public PlayerMovementWithDash playerMove;
+    public DialogueManager dialManager;
+    public DialogueTrigger dialTrigger;
+    public Bullet bullet;
+    public Enemystuff enemy;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
