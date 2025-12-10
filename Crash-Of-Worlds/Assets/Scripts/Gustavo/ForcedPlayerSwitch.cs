53df8c32121301f61842d2ff5dc46a756541796d
MUKILTEO\1725468 <ceballosg365@gmail.com>
2025-12-10 10:14:50 -0800

    yes

--- Crash-Of-Worlds/Assets/Scripts/Gustavo/ForcedPlayerSwitch.cs
+++ Crash-Of-Worlds/Assets/Scripts/Gustavo/ForcedPlayerSwitch.cs
@@ -100,4 +100,6 @@ public class ForcedPlayerSwitch : MonoBehaviour
 
         yield break;
     }
+
+    
 }
--- Crash-Of-Worlds/Assets/Scripts/Gustavo/PauseMenu1.cs
+++ Crash-Of-Worlds/Assets/Scripts/Gustavo/PauseMenu1.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +7,11 @@ public class PauseMenu1 : MonoBehaviour
 
 {
     public GameObject pauseMenu;
+    public GameObject pauseMenu2;
+    public bool player1Active;
+    public GameObject avatar1, avatar2;
+   
+
 
     void Start()
     {
@@ -13,15 +19,42 @@ public class PauseMenu1 : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab) && player1Active == true)
         {
             pauseMenu.SetActive(!pauseMenu.activeSelf);
             Time.timeScale = 0;
-
         }
-
+        else if (Input.GetKeyDown(KeyCode.Tab) && player1Active == false)
+        {
+            pauseMenu2.SetActive(!pauseMenu2.activeSelf);
+            Time.timeScale = 0;
+        }
     }
+    public void MenuSwitch()
+    {
+        if (player1Active)
+        {
+            // Switch from avatar1 -> avatar2
+            if (avatar1 != null) avatar1.SetActive(false);
+            if (avatar2 != null) avatar2.SetActive(true);
 
+            player1Active = false;
+
+            
+        }
+        else
+        {
+            // Switch from avatar2 -> avatar1
+            if (avatar1 != null) avatar1.SetActive(true);
+            if (avatar2 != null) avatar2.SetActive(false);
+
+           
+
+            player1Active = true;
+
+           
+        }
+    }
     public void Home()
     {
         SceneManager.LoadScene("StartScene");
@@ -31,6 +64,7 @@ public class PauseMenu1 : MonoBehaviour
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        pauseMenu2.SetActive(false);
         Time.timeScale = 1;
     }
 
