d379463ab623cb191f9d96c8a895d0b62f50c4f4
child\Benjamin <1725203@mukilteo.wednet.edu>
2026-01-06 00:26:19 -0800

    finished... (so cooked) (made a build and sent it to derek, couldnt get on itch cuz i couldnt log in)(sent via gmail)

--- Crash-Of-Worlds/Assets/MainMenuScript.cs
+++ Crash-Of-Worlds/Assets/MainMenuScript.cs
@@ -5,11 +5,26 @@ public class MainMenuScript : MonoBehaviour
 {
     public GameObject MainMenuStuff;
     public GameObject CreditsStuff;
+    public GameObject LaeCredits;
+    public GameObject BenCredits;
+    public GameObject GabeCredits;
+    public GameObject BrielleCredits;
+    public GameObject GeorgeCredits;
+    public GameObject TylerCredits;
+    public GameObject GustavoCredits;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MainMenuStuff.SetActive(true);
         CreditsStuff.SetActive(false);
+        LaeCredits.SetActive(false);
+        BenCredits.SetActive(false);
+        GabeCredits.SetActive(false);
+        BrielleCredits.SetActive(false);
+        GeorgeCredits.SetActive(false);
+        TylerCredits.SetActive(false);
+        GustavoCredits.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,6 +42,13 @@ public class MainMenuScript : MonoBehaviour
     {
         MainMenuStuff.SetActive(false);
         CreditsStuff.SetActive(true);
+        LaeCredits.SetActive(false);
+        BenCredits.SetActive(false);
+        GabeCredits.SetActive(false);
+        BrielleCredits.SetActive(false);
+        GeorgeCredits.SetActive(false);
+        TylerCredits.SetActive(false);
+        GustavoCredits.SetActive(false);
     }
 
     public void MainMenu()
@@ -35,6 +57,45 @@ public class MainMenuScript : MonoBehaviour
         CreditsStuff.SetActive(false);
     }
 
+    public void Lae()
+    {
+        CreditsStuff.SetActive(false);
+        LaeCredits.SetActive(true);
+    }
+
+    public void Ben()
+    {
+        CreditsStuff.SetActive(false);
+        BenCredits.SetActive(true);
+    }
+    public void Gabe()
+    {
+        CreditsStuff.SetActive(false);
+        GabeCredits.SetActive(true);
+    }
+
+    public void Brielle()
+    {
+        CreditsStuff.SetActive(false);
+        BrielleCredits.SetActive(true);
+    }
+
+    public void George()
+    {
+        CreditsStuff.SetActive(false);
+        GeorgeCredits.SetActive(true);
+    }
+
+    public void Tyler()
+    {
+        CreditsStuff.SetActive(false);
+        TylerCredits.SetActive(true);
+    }
+    public void Gustavo()
+    {
+        CreditsStuff.SetActive(false);
+        GustavoCredits.SetActive(true);
+    }
     public void QuitGame()
     {
         Application.Quit();
